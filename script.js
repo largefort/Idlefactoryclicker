02411a93c1fec06a3475d77b507f49556f8e3500
largefort <106451813+largefort@users.noreply.github.com>
2023-07-12 16:58:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,129 +1,72 @@
-// Game state
+// Get elements
+const moneyElem = document.getElementById("money");
+const factoriesElem = document.getElementById("factories");
+const machinesElem = document.getElementById("machines");
+const clickBtn = document.getElementById("clickBtn");
+const buyFactoryBtn = document.getElementById("buyFactoryBtn");
+const buyMachineBtn = document.getElementById("buyMachineBtn");
+
+// Game state variables
 let money = 0;
 let factories = 0;
 let machines = 0;
-let factoryProduction = 1;
-let machineProduction = 10;
 
-// Factory and machine names
-let factoryNames = [];
-let machineNames = [];
+// Update the display
+function updateDisplay() {
+  moneyElem.textContent = money;
+  factoriesElem.textContent = factories;
+  machinesElem.textContent = machines;
 
-// Get saved game state from local storage
-const savedGame = localStorage.getItem("idleFactoryClickerSave");
-if (savedGame) {
-  const gameData = JSON.parse(savedGame);
-  money = gameData.money;
-  factories = gameData.factories;
-  machines = gameData.machines;
-  factoryNames = gameData.factoryNames || [];
-  machineNames = gameData.machineNames || [];
+  // Disable buttons if not enough money
+  buyFactoryBtn.disabled = money < 100;
+  buyMachineBtn.disabled = money < 10;
 }
 
-// Update the game state on the screen
-function updateGameState() {
-  document.getElementById("money").textContent = money;
-  document.getElementById("factories").innerHTML = "";
-  for (let i = 0; i < factories; i++) {
-    const factoryName = factoryNames[i] || `Factory ${i + 1}`;
-    document.getElementById("factories").innerHTML += `<div class="factory">${factoryName}</div>`;
-  }
-  document.getElementById("machines").innerHTML = "";
-  for (let i = 0; i < machines; i++) {
-    const machineName = machineNames[i] || `Machine ${i + 1}`;
-    document.getElementById("machines").innerHTML += `<div class="machine">${machineName}</div>`;
-  }
-}
-
-// Function to earn money by clicking
-function clickMoney() {
+// Produce money when the click button is clicked
+function produce() {
   money += 1;
-  updateGameState();
+  updateDisplay();
 }
 
-// Function to buy a factory
+// Buy a factory
 function buyFactory() {
-  if (money >= 10) {
-    money -= 10;
-    factories += 1;
-    factoryNames.push(prompt("Enter a name for the factory:") || `Factory ${factories}`);
-    updateGameState();
-  } else {
-    alert("Not enough money to buy a factory!");
-  }
-}
-
-// Function to add a factory without spending money
-function addFactory() {
-  factories += 1;
-  factoryNames.push(prompt("Enter a name for the factory:") || `Factory ${factories}`);
-  updateGameState();
-}
-
-// Function to buy a machine
-function buyMachine() {
   if (money >= 100) {
     money -= 100;
-    machines += 1;
-    machineNames.push(prompt("Enter a name for the machine:") || `Machine ${machines}`);
-    updateGameState();
-  } else {
-    alert("Not enough money to buy a machine!");
+    factories += 1;
+    updateDisplay();
   }
 }
 
-// Function to add a machine without spending money
-function addMachine() {
-  machines += 1;
-  machineNames.push(prompt("Enter a name for the machine:") || `Machine ${machines}`);
-  updateGameState();
+// Buy a machine
+function buyMachine() {
+  if (money >= 10) {
+    money -= 10;
+    machines += 1;
+    updateDisplay();
+  }
 }
 
-// Function to automate factory production
-function automateFactory() {
-  money += factories * factoryProduction;
-  updateGameState();
-}
-
-// Function to automate machine production
-function automateMachine() {
-  money += machines * machineProduction;
-  updateGameState();
-}
-
-// Function to save the game state to local storage
+// Save game progress to local storage
 function saveGame() {
-  const gameData = {
+  const saveData = {
     money: money,
     factories: factories,
-    machines: machines,
-    factoryNames: factoryNames,
-    machineNames: machineNames
+    machines: machines
   };
-  localStorage.setItem("idleFactoryClickerSave", JSON.stringify(gameData));
-  alert("Game saved!");
+  localStorage.setItem("idleFactorySave", JSON.stringify(saveData));
 }
 
-// Function to load the game state from local storage
+// Load game progress from local storage
 function loadGame() {
-  const savedGame = localStorage.getItem("idleFactoryClickerSave");
-  if (savedGame) {
-    const gameData = JSON.parse(savedGame);
-    money = gameData.money;
-    factories = gameData.factories;
-    machines = gameData.machines;
-    factoryNames = gameData.factoryNames || [];
-    machineNames = gameData.machineNames || [];
-    updateGameState();
-    alert("Game loaded!");
-  } else {
-    alert("No saved game found!");
+  const saveData = JSON.parse(localStorage.getItem("idleFactorySave"));
+  if (saveData) {
+    money = saveData.money;
+    factories = saveData.factories;
+    machines = saveData.machines;
+    updateDisplay();
   }
 }
 
-// Set intervals for automated production
-setInterval(automateFactory, 1000); // Automate factory production every 1 second
-setInterval(automateMachine, 100); // Automate machine production every 100 milliseconds
-
-// Initial update of the game state
-updateGameState();
+// Event listeners for save and load buttons
+window.addEventListener("beforeunload", saveGame);
+window.addEventListener("load", loadGame);
