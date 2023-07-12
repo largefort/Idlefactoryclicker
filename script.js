7db0f28deb99ed3651e7867322398811b77468b3
largefort <106451813+largefort@users.noreply.github.com>
2023-07-12 16:34:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 let money = 0;
 let factories = 0;
 let machines = 0;
+let factoryProduction = 1;
+let machineProduction = 10;
+
+// Factory and machine names
+let factoryNames = [];
+let machineNames = [];
 
 // Get saved game state from local storage
 const savedGame = localStorage.getItem("idleFactoryClickerSave");
@@ -10,6 +16,8 @@ if (savedGame) {
   money = gameData.money;
   factories = gameData.factories;
   machines = gameData.machines;
+  factoryNames = gameData.factoryNames || [];
+  machineNames = gameData.machineNames || [];
 }
 
 // Update the game state on the screen
@@ -17,11 +25,13 @@ function updateGameState() {
   document.getElementById("money").textContent = money;
   document.getElementById("factories").innerHTML = "";
   for (let i = 0; i < factories; i++) {
-    document.getElementById("factories").innerHTML += `<div class="factory">Factory ${i + 1}</div>`;
+    const factoryName = factoryNames[i] || `Factory ${i + 1}`;
+    document.getElementById("factories").innerHTML += `<div class="factory">${factoryName}</div>`;
   }
   document.getElementById("machines").innerHTML = "";
   for (let i = 0; i < machines; i++) {
-    document.getElementById("machines").innerHTML += `<div class="machine">Machine ${i + 1}</div>`;
+    const machineName = machineNames[i] || `Machine ${i + 1}`;
+    document.getElementById("machines").innerHTML += `<div class="machine">${machineName}</div>`;
   }
 }
 
@@ -36,6 +46,7 @@ function buyFactory() {
   if (money >= 10) {
     money -= 10;
     factories += 1;
+    factoryNames.push(prompt("Enter a name for the factory:") || `Factory ${factories}`);
     updateGameState();
   } else {
     alert("Not enough money to buy a factory!");
@@ -47,10 +58,23 @@ function buyMachine() {
   if (money >= 100) {
     money -= 100;
     machines += 1;
+    machineNames.push(prompt("Enter a name for the machine:") || `Machine ${machines}`);
     updateGameState();
   } else {
     alert("Not enough money to buy a machine!");
   }
+}
+
+// Function to automate factory production
+function automateFactory() {
+  money += factories * factoryProduction;
+  updateGameState();
+}
+
+// Function to automate machine production
+function automateMachine() {
+  money += machines * machineProduction;
+  updateGameState();
 }
 
 // Function to save the game state to local storage
@@ -58,7 +82,9 @@ function saveGame() {
   const gameData = {
     money: money,
     factories: factories,
-    machines: machines
+    machines: machines,
+    factoryNames: factoryNames,
+    machineNames: machineNames
   };
   localStorage.setItem("idleFactoryClickerSave", JSON.stringify(gameData));
   alert("Game saved!");
@@ -72,12 +98,18 @@ function loadGame() {
     money = gameData.money;
     factories = gameData.factories;
     machines = gameData.machines;
+    factoryNames = gameData.factoryNames || [];
+    machineNames = gameData.machineNames || [];
     updateGameState();
     alert("Game loaded!");
   } else {
     alert("No saved game found!");
   }
 }
+
+// Set intervals for automated production
+setInterval(automateFactory, 1000); // Automate factory production every 1 second
+setInterval(automateMachine, 100); // Automate machine production every 100 milliseconds
 
 // Initial update of the game state
 updateGameState();
